61a353e98f60c3acd663ac596127d7c9d21b622e
Martina Veleva <m.veleva93@gmail.com>
2022-11-20 23:16:55 +0200

    client - material module

---
+++ client/src/app/material/material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+const MaterialComponents = [
+  MatFormFieldModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatInputModule,
+  MatIconModule,
+  MatSnackBarModule,
+  MatProgressBarModule,
+  MatDialogModule,
+  MatCardModule,
+  MatSelectModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTableModule,
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+})
+
+export class MaterialModule {}
