808a9540dbc0a134abebf1009ef85206ddb7fbfc
Martina Veleva <m.veleva93@gmail.com>
2022-11-26 02:55:35 +0200

    added db context

---
+++ API/Data/CelestialDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+      public class CelestialDbContext : DbContext
+      {
+            public CelestialDbContext(DbContextOptions options) : base(options)
+            {
+                this.Database.Migrate();
+            }
+            public DbSet<Article> Articles { get; set; }
+      }
+}
